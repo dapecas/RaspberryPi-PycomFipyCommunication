311c9d08cd8d127cfe388ea52289003229eb1387
dapecas <99323067+dapecas@users.noreply.github.com>
2022-07-18 17:09:44 +0200

    Create receiveDataFromRaspi.py

---
+++ sendStrRaspiToFipy/receiveDataFromRaspi.py
@@ -0,0 +1,26 @@
+#code to use in raspi: enviarHolaMundo.py
+
+#Program that collects the content of a str and saves it in a TXT.
+
+import time
+import pycom as py
+from machine import UART
+
+txtContent=None
+
+uart1=UART(1, 115200)
+uart1.init(115200, bits=8, parity=None, stop=1, pins=('P3', 'P4'))
+
+while txtContent==None:
+    if uart1.any():
+        txtContent=uart1.read(10).decode('utf-8') #Voy a buscar si necesita un waiting para leer
+        time.sleep(1)
+    else:
+        print('No ha recogido nada')
+        time.sleep(1)
+
+with open('/flash/prueba.txt', 'w') as data:  #Create a txt file to save all data
+    data.write(txtContent)
+    uart1.deinit()
+    py.rgbled(0x00FF00) #verde
+    data.close()
