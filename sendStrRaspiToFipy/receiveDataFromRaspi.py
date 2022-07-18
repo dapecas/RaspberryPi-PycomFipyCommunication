e2c7942d6b41bd3bbe7bf13dcbea1d7d06b9a31f
dapecas <99323067+dapecas@users.noreply.github.com>
2022-07-18 17:12:35 +0200

    Update receiveDataFromRaspi.py

--- sendStrRaspiToFipy/receiveDataFromRaspi.py
+++ sendStrRaspiToFipy/receiveDataFromRaspi.py
@@ -1,5 +1,4 @@
 #code to use in raspi: enviarHolaMundo.py
-
 #Program that collects the content of a str and saves it in a TXT.
 
 import time
@@ -9,11 +8,11 @@ from machine import UART
 txtContent=None
 
 uart1=UART(1, 115200)
-uart1.init(115200, bits=8, parity=None, stop=1, pins=('P3', 'P4'))
+uart1.init(115200, bits=8, parity=None, stop=1, pins=('P3', 'P4'))  #Initialization of UART port 1
 
 while txtContent==None:
     if uart1.any():
-        txtContent=uart1.read(10).decode('utf-8') #Voy a buscar si necesita un waiting para leer
+        txtContent=uart1.read(10).decode('utf-8')   #Fipy read only 10 characters
         time.sleep(1)
     else:
         print('No ha recogido nada')
@@ -22,5 +21,5 @@ while txtContent==None:
 with open('/flash/prueba.txt', 'w') as data:  #Create a txt file to save all data
     data.write(txtContent)
     uart1.deinit()
-    py.rgbled(0x00FF00) #verde
+    py.rgbled(0x00FF00) #Green
     data.close()
