ca261c231e3f094574834af9b073fbe02719f73f
dapecas <99323067+dapecas@users.noreply.github.com>
2022-07-19 10:26:11 +0200

    Update sendStrToRaspi.py

--- sendStrFipyToRaspi/sendStrToRaspi.py
+++ sendStrFipyToRaspi/sendStrToRaspi.py
@@ -1,1 +1,15 @@
-import
+import time
+import pycom
+from machine import UART
+
+pycom.heartbeat(False)
+
+uart1=UART(1, 115200)
+uart1.init(115200, bits=8, parity=None, stop=1, pins=('P3', 'P4'))
+
+x=1
+
+while True:
+    uart1.write(b'Hello')
+    print('Sent message number: '+str(x))
+    x+=1
